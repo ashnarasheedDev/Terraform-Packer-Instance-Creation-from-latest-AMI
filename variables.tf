8e0aa40de4da42f81794653cdf0c03e9cad8076a
Ashna Rasheed <128576349+ashnarasheedDev@users.noreply.github.com>
2023-05-17 14:20:45 +0530

    Create variables.tf
    By defining these variables with default values, you can use them throughout your Terraform configuration to create reusable and configurable infrastructure. You can reference these variables using the var prefix, for example, var.env, var.project_name, etc., wherever you need to use their values in your Terraform resources or modules.

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "env" {
+  default = "dev"
+}
+ 
+variable "project_name" {
+  default = "my-project"
+}
+ 
+variable "region" {
+  default = "ap-south-1"
+}
+ 
+ 
+variable "instance_type" {
+  default = "t2.micro"
+}
+ 
+variable "domain_name" {
+  default = "ashna.online"
+}
+ 
+variable "record_name" {
+  default = "webserver-mumbai"
+}
